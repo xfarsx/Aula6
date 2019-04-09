1e7e8bde26ab0cb7bb8f4a50b8c529cb11ff47c5
xfarsx <fernando.arsantos@gmail.com>
2019-04-08 21:45:04 -0300

    Initial commit

---
+++ src/br/com/digitalhouse/Principal.java
@@ -0,0 +1,12 @@
+package br.com.digitalhouse;
+
+public class Principal {
+
+
+    public static void main(String [] args){
+
+    Cachorro cachorro = new Cachorro ("Bidu","Grande", "Pitbull");
+        System.out.println("Esse cachorro é mamífero? " + cachorro.ehMamifero());
+
+                }
+}
